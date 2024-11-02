099567e7550b1d803abb4f4c0d18989dec19e51f
Kanin Gleb <gleb.kanin@gmail.com>
2024-11-02 19:45:43 +0300

    JS adaptation from 0 to 680, 680 to infinity added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 const sliderMain = new Swiper('.slider_main', {
-    
+    freeMode: true,
+    centerSlides: true,
+    breakPoins: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 20
+        },
+        680 : {
+            slidesPerView: 3.5,
+            spaceBetween: 60
+        }
+    }
 })
